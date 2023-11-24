eb69a683faf98cdea27a8814d882b628033b48fb
Fabio Silva <fabiolazera.lazera@gmail.com>
2023-11-24 21:55:54 +0000

    Navbar updated

--- E-letRonics_frontend/src/components/navbar.vue
+++ E-letRonics_frontend/src/components/navbar.vue
@@ -3,9 +3,11 @@
 </script>
 
 <template>
-    <div class="navbar bg-red-500">
+    <div class="navbar">
         <div class="flex-1">
-            <a class="btn btn-ghost text-xl">daisyUI</a>
+            <a class="btn btn-ghost text-xl">Home</a>
+            <a class="btn btn-ghost text-xl">About Us</a>
+            <a class="btn btn-ghost text-xl">Contact</a>
         </div>
         <div class="flex-none">
             <div class="dropdown dropdown-end">
@@ -18,33 +20,7 @@
                         </svg>
                         <span class="badge badge-sm indicator-item">8</span>
                     </div>
-                </label>
-                <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-                    <div class="card-body">
-                        <span class="font-bold text-lg">8 Items</span>
-                        <span class="text-info">Subtotal: $999</span>
-                        <div class="card-actions">
-                            <button class="btn btn-primary btn-block">View cart</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="dropdown dropdown-end">
-                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                    <div class="w-10 rounded-full">
-                        <img alt="Tailwind" src="" />
-                    </div>
-                </label>
-                <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                    <li>
-                        <a class="justify-between">
-                            Profile
-                            <span class="badge">New</span>
-                        </a>
-                    </li>
-                    <li><a>Settings</a></li>
-                    <li><a>Logout</a></li>
-                </ul>
+                </label>               
             </div>
         </div>
     </div>
