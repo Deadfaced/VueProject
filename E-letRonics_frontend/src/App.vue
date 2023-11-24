2db70f6768f9e752f865afcb41f7115d6acfcde6
Claudio <claudio.silva.t0123173@edu.atec.pt>
2023-11-24 23:15:21 +0000

    Grid product gallery bugfix

--- E-letRonics_frontend/src/App.vue
+++ E-letRonics_frontend/src/App.vue
@@ -2,8 +2,6 @@
 
 import Navbar from './components/navbar.vue';
 import Hero from './components/hero.vue';
-import ContactForm from './components/ContactForm.vue';
-import Footer from './components/Footer.vue';
 import ProductGrid from './components/ProductGrid.vue';
 import TestimonialCard from './components/TestimonialCard.vue';
 import CardCart from './components/CardCart.vue';
@@ -12,13 +10,14 @@ import ContactPage from './components/ContactPage.vue';
 </script>
 
 <template>
-    <Navbar></Navbar>
-    <Hero></Hero>
-    <ProductGrid></ProductGrid>
-    <TestimonialCard></TestimonialCard>
-    <ContactPage></ContactPage>
-    <Footer></Footer>
-
+    <div>
+        <Navbar></Navbar>
+        <Hero></Hero>
+        <ProductGrid></ProductGrid>
+        <TestimonialCard></TestimonialCard>
+        <CardCart></CardCart>
+        <ContactPage></ContactPage>
+    </div>
 </template>
 
 <style scoped></style>
