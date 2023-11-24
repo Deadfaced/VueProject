c4acd2c5abdf419128f7788702eb82e7eb238478
daniel pereira <danielpereira22costa@gmail.com>
2023-11-24 23:15:35 +0000

    Done minor changes on TestimonialCard

--- E-letRonics_frontend/src/App.vue
+++ E-letRonics_frontend/src/App.vue
@@ -1,18 +1,14 @@
 <script setup>
 
-import Navbar from './components/navbar.vue';
-import Hero from './components/hero.vue';
-import ContactForm from './components/ContactForm.vue';
-import Footer from './components/Footer.vue';
-import ProductGrid from './components/ProductGrid.vue';
+import navbar from './components/navbar.vue';
+import hero from './components/hero.vue';
+import Product from './components/Product.vue';
 import TestimonialCard from './components/TestimonialCard.vue';
-import CardCart from './components/CardCart.vue';
-
+import Footer from './components/Footer.vue';
 
 </script>
 
 <template>
-  
 
 </template>
 
