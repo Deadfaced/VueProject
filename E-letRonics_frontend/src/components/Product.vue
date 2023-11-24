ee2ab6db48e32f80e99615aecf59a39d26be9540
Claudio <claudio.silva.t0123173@edu.atec.pt>
2023-11-24 22:34:21 +0000

    Product card updated

--- E-letRonics_frontend/src/components/Product.vue
+++ E-letRonics_frontend/src/components/Product.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="card-container">
+        <div class="flex justify-center items-center mx-auto">
       <div class="card glass">
         <figure>
-          <img src="https://static.pcdiga.com/media/catalog/product/cache/4a9972e1440204cef7cf19ceb7c4fc35/2/_/2_p061426.jpg" alt="PC" />
+          <img class="w-full h-auto" src="https://static.pcdiga.com/media/catalog/product/cache/4a9972e1440204cef7cf19ceb7c4fc35/2/_/2_p061426.jpg" alt="PC" />
         </figure>
-        <div class="card-body">
-          <h2 class="card-title">Life Hack</h2>
+        <div class="flex flex-col justify-between items-center w-full h-full p-4">
+          <h2 class="m-0 p-0 text-2xl font-bold text-center">Life Hack</h2>
           <p>Discover the art of parking your car in your garage with ease.</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Learn Now!</button>
+          <div class="flex flex-col justify-between items-center w-full mt-4">
+            <button class="w-full py-2 px-4 rounded-lg focus:outline-none cursor-pointer bg-gray-600 hover:bg-gray-400 text-white font-semibold mb-2">Learn Now!</button>
             <Rating></Rating>
           </div>
         </div>
@@ -30,77 +30,13 @@
   </script>
   
   <style scoped>
-  .card-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    max-width: 600px;
-    margin: 0 auto;
-    height: 100vh;
-  }
-  
   .card {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem;
-    border-radius: 0.5rem;
-    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.2);
-    background-color: rgba(255, 255, 255, 0.1);
-    backdrop-filter: blur(0.5rem);
     transition: all 0.3s ease-in-out;
-    width: 100%;
   }
-  
   .card:hover {
-    transform: scale(1.05);
-  }
-  
-  .card figure img {
-    width: 100%;
-    height: auto;
-    object-fit: cover;
-  }
-  
-  .card-body {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    padding: 1rem;
-  }
-  
-  .card-title {
-    margin: 0;
-    padding: 0;
-    font-size: 1.5rem;
-    font-weight: 700;
-    text-align: center;
-  }
-  
-  .card-actions {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    margin-top: 1rem;
-  }
-  
+    transform: scale(1.1);
+  } 
   .btn {
-    width: 100%;
-    padding: 0.5rem 1rem;
-    border-radius: 0.5rem;
-    border: none;
-    outline: none;
-    cursor: pointer;
     transition: all 0.3s ease-in-out;
-  }
-  
-  .Rating {
-    text-align: center;
   }
   </style>
