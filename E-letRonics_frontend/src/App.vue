d0e594a2176eb768840a5dd1ce5c362bcbc7ca1b
daniel pereira <danielpereira22costa@gmail.com>
2023-11-24 21:38:31 +0000

    Changed gitignore

--- E-letRonics_frontend/src/App.vue
+++ E-letRonics_frontend/src/App.vue
@@ -1,30 +1,9 @@
 <script setup>
 
-import Navbar from './components/navbar.vue';
-import Hero from './components/hero.vue';
-import ContactForm from './components/ContactForm.vue';
-import Footer from './components/Footer.vue';
-<<<<<<< HEAD
-import ProductGrid from './components/ProductGrid.vue';
-=======
-import Product from './components/Product.vue';
-import TestimonialCard from './components/TestimonialCard.vue';
-import CardCart from './components/CardCart.vue';
-
->>>>>>> c566bff9e198c9fc3939a16e13582de2aca9a66e
-
 </script>
 
 <template>
-  <Navbar />
-<<<<<<< HEAD
-  <ProductGrid></ProductGrid>
-=======
-  <card-cart/>
->>>>>>> c566bff9e198c9fc3939a16e13582de2aca9a66e
-  <ContactForm/>
-  <Hero/>
-  <Footer></Footer>
+
 </template>
 
 <style scoped></style>
