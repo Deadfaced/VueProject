4802df4dbfadff6606daba7e044445bf5df19485
Claudio <claudio.silva.t0123173@edu.atec.pt>
2023-11-24 19:32:27 +0000

    Footer Component

--- E-letRonics_frontend/src/App.vue
+++ E-letRonics_frontend/src/App.vue
@@ -3,6 +3,7 @@
 import Navbar from './components/navbar.vue';
 import Hero from './components/hero.vue';
 import ContactForm from './components/ContactForm.vue';
+import Footer from './components/Footer.vue';
 
 </script>
 
@@ -10,6 +11,7 @@ import ContactForm from './components/ContactForm.vue';
   <Navbar />
   <ContactForm/>
   <Hero />
+  <Footer></Footer>
 </template>
 
 <style scoped></style>
