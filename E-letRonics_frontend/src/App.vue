3d532e9a448c94f6fca29827350a448a5b885b59
Fabio Silva <fabiolazera.lazera@gmail.com>
2023-11-24 20:58:06 +0000

    CardCart added

--- E-letRonics_frontend/src/App.vue
+++ E-letRonics_frontend/src/App.vue
@@ -4,13 +4,15 @@ import Navbar from './components/navbar.vue';
 import Hero from './components/hero.vue';
 import ContactForm from './components/ContactForm.vue';
 import Footer from './components/Footer.vue';
+import CardCart from './components/CardCart.vue';
 
 </script>
 
 <template>
   <Navbar />
+  <card-cart/>
   <ContactForm/>
-  <Hero />
+  <Hero/>
   <Footer></Footer>
 </template>
 
---
+++ E-letRonics_frontend/src/components/CardCart.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="max-w-md mx-auto">
+    <div class="card card-side bg-base-100 shadow-xl p-4 mt-4 flex">
+      <figure class="flex-none mr-4">
+        <img src="https://pplware.sapo.pt/wp-content/uploads/2022/12/GeForce-GTX-1650.jpg" alt="Item" class="w-40 h-auto" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title text-sm">{{ Title }} | {{ Description }}</h2>
+        <h2 class="card-title text-sm">{{ Category }}</h2>
+        <h2 class="card-title text-sm">Qty: {{ Qty }}</h2>
+        <h2 class="card-title text-sm">Unit Cost: {{ Price }}</h2>
+        <h2 class="card-title text-sm">In stock</h2>
+        <h2 class="card-title text-sm">Total: 600€</h2>
+        <div class="card-actions justify-end">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+  
+
+<script>
+export default {
+    data() {
+        return {
+            Title : 'GeForce-GTX-1650',
+            Category: 'Graphics Card',
+            Description: '4GB ',
+            Qty: '2',
+            Price : '300€'
+        };
+    },
+}
+</script>
+
+<style>
+
+</style>
