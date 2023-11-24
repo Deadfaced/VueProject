631118d215767e1a2b2c5d0823e4661272aa9b88
Claudio <claudio.silva.t0123173@edu.atec.pt>
2023-11-24 19:48:36 +0000

    Product Grid Component

--- E-letRonics_frontend/src/App.vue
+++ E-letRonics_frontend/src/App.vue
@@ -4,11 +4,13 @@ import Navbar from './components/navbar.vue';
 import Hero from './components/hero.vue';
 import ContactForm from './components/ContactForm.vue';
 import Footer from './components/Footer.vue';
+import Product from './components/Product.vue';
 
 </script>
 
 <template>
   <Navbar />
+  <Product></Product>
   <ContactForm/>
   <Hero />
   <Footer></Footer>
---
+++ E-letRonics_frontend/src/components/Product.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card w-96 glass">
+    <figure><img src="https://static.pcdiga.com/media/catalog/product/cache/4a9972e1440204cef7cf19ceb7c4fc35/2/_/2_p061426.jpg" alt="PC"/></figure>
+    <div class="card-body">
+        <h2 class="card-title">Life hack</h2>
+        <p>How to park your car at your garage?</p>
+        <div class="card-actions justify-end">
+        <button class="btn btn-primary">Learn now!</button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
