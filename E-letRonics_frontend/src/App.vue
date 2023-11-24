322553f6e22c69397c92b6733d7d653154f154c6
Claudio <claudio.silva.t0123173@edu.atec.pt>
2023-11-24 19:28:01 +0000

    Contact Form Component

--- E-letRonics_frontend/src/App.vue
+++ E-letRonics_frontend/src/App.vue
@@ -2,11 +2,13 @@
 
 import Navbar from './components/navbar.vue';
 import Hero from './components/hero.vue';
+import ContactForm from './components/ContactForm.vue';
 
 </script>
 
 <template>
   <Navbar />
+  <ContactForm/>
   <Hero />
 </template>
 
