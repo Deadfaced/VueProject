497608bac7fc9a87f10a3128a7dd70f6f3f5223c
Bernardo Teixeira <btteixeira.bt@gmail.com>
2023-11-28 16:47:04 +0000

    removed everything from backend

--- E-letRonics_frontend/src/App.vue
+++ E-letRonics_frontend/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import Product from './components/Product/Product.vue';
-
+import CardCart from './components/CardCart.vue';
+import SideCart from './components/SideCart.vue';
 
 </script>
 
 <template>
-    <Product></Product>
+    <SideCart></SideCart>
+    <!-- <CardCart></CardCart> -->
+    <!-- <Product></Product> -->
 </template>
 
 <style scoped></style>
