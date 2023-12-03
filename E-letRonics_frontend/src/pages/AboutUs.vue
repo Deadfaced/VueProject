5742b53906d9300a7119b7abd087dae2c030e544
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2023-12-03 21:09:49 +0000

    AboutUs page implemented

---
+++ E-letRonics_frontend/src/pages/AboutUs.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="text-white min-h-screen flex items-center  bg-cover" style="background-image: url('https://images.unsplash.com/photo-1529526013826-d9eddeb1bc9e?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');">
+      <div class="max-w-lg w-1/2 bg-black bg-opacity-70 p-8 rounded-md">
+        <h1 class="text-4xl md:text-6xl font-semibold leading-normal mb-6">E-letRonics</h1>
+        <p class="mb-4">
+          We are a eletrical components store created by four students passionate about the world of technology. We combine academic knowledge and enthusiasm to offer the best products and an exceptional shopping experience.
+        </p>
+        <p class="mb-4">
+          Explore our catalog and discover the quality that only a committed team can provide.
+        </p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    setup() {
+      return {};
+    },
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
