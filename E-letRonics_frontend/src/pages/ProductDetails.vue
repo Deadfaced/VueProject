517a9d32fd605a2f05892bee0e4caf7b39e727d9
Claudio <claudio.silva.t0123173@edu.atec.pt>
2023-12-04 21:24:10 +0000

    Planning excalidraw updated

--- E-letRonics_frontend/src/pages/ProductDetails.vue
+++ E-letRonics_frontend/src/pages/ProductDetails.vue
@@ -1,6 +1,36 @@
 <template>
-  <h1>{{ this.$route.params.id }}</h1>
-  
+    <h1>{{ this.$route.params.id }}</h1>
+  <div class="flex justify-center items-center mx-auto p-8">
+    <div class="flex">
+      <div class="w-1/2 pr-8">
+        <img src="https://buffer.com/library/content/images/2023/09/instagram-image-size.jpg" alt="Product Image" />
+      </div>
+
+      <div class="w-1/2">
+        <h2 class="text-3xl font-semibold mb-4">{{ product.name }}</h2>
+        <p class="text-gray-600 mb-4">{{ product.description }}</p>
+        <div class="flex items-center mb-4">
+          <span class="text-xl font-semibold text-gray-800 mr-2">${{ product.price }}</span>
+          <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded">
+            <h1>rating</h1>
+          </span>
+        </div>
+
+        <!-- Controles de Quantidade -->
+        <div class="flex items-center mb-4">
+          <button @click="decreaseQuantity" class="quantity-button">-</button>
+          <span class="mx-2 text-lg">{{ quantity }}</span>
+          <button @click="increaseQuantity" class="quantity-button">+</button>
+        </div>
+
+        <!-- Botões de Ação -->
+        <div class="flex">
+          <button @click="addToCart" class="action-button bg-blue-600 text-white">Add to Cart</button>
+          <button @click="showMoreDetails" class="action-button bg-gray-300 text-gray-700 ml-2">More Details</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +38,7 @@ export default {
   data() {
     return {
       quantity: 1,
+      product: null,
     };
   },
   methods: {
@@ -25,8 +56,17 @@ export default {
     showMoreDetails() {
       // Implementar a lógica para mostrar mais detalhes do produto
     },
+    // Método para buscar os detalhes do produto com base no ID da rota
+    fetchProductDetails() {
+      const productId = this.$route.params.id;
+      // Substitua a chamada abaixo pela lógica real para buscar os detalhes do produto
+      // Exemplo: this.product = productService.getProductDetails(productId);
+    },
   },
-  props: ['item'],
+  // Use o hook created para chamar o método fetchProductDetails quando o componente for criado
+  created() {
+    this.fetchProductDetails();
+  },
 };
 </script>
 
