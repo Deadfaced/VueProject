efc33372eadce196dca356f05073af6fd349eb70
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2023-12-05 10:36:48 +0000

    About page implemented with components and added responsiveness

--- E-letRonics_frontend/src/components/AboutUs/Aboutcard.vue
+++ E-letRonics_frontend/src/components/AboutUs/Aboutcard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="max-w-screen-md mx-auto p-8 text-white">
-      <h1 class="text-4xl md:text-6xl font-semibold leading-normal mb-6 neon-bounce-text">E-letRonics</h1>
+      <h1 class="text-4xl md:text-6xl font-semibold leading-normal text-center mb-6 neon-bounce-text">E-letRonics</h1>
       <p class="mb-4">
         We are an electrical components store created by four students passionate about the world of technology. We combine academic knowledge and enthusiasm to offer the best products and an exceptional shopping experience.
       </p>
--- E-letRonics_frontend/src/components/AboutUs/MissionCard.vue
+++ E-letRonics_frontend/src/components/AboutUs/MissionCard.vue
@@ -2,7 +2,7 @@
     <div class="group cursor-pointer transition-opacity text-white">
       <div class="p-4 rounded-md opacity-0 group-hover:opacity-100 transition-opacity">
         <h2 class="text-xl md:text-2xl lg:text-3xl font-semibold mb-2 neon-bounce-text">Mission</h2>
-        <p class="text-sm md:text-base lg:text-lg">
+        <p class="text-sm md:text-base">
           Enhance connectivity and facilitate access to innovation, offering electronic products of exceptional quality that enrich people's lives, promote efficiency and inspire technological evolution, always focusing on sustainability and customer satisfaction.
         </p>
       </div>
