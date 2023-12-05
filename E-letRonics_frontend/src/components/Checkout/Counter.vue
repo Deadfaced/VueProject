60e38afbe98d0e8ade25f799f7c93b46ca8b09d9
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2023-12-05 15:29:10 +0000

    Bug fixed on CheckoutCard

--- E-letRonics_frontend/src/components/Checkout/Counter.vue
+++ E-letRonics_frontend/src/components/Checkout/Counter.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mt-8 flex flex-col sm:flex-row">
+    <div class="flex flex-col sm:flex-row">
         <button @click="decreaseQuantity" class="mr-4 mb-4 flex cursor-pointer items-center justify-center rounded-md border py-2 px-8 text-center text-gray-500 transition duration-150 ease-in-out hover:translate-y-1 hover:bg-rose-500 hover:text-white">-</button>
         <h1 class="text-2xl text-white">{{ quantity }}</h1>
         <button @click="increaseQuantity" class="mr-4 ml-4 mb-4 flex cursor-pointer items-center justify-center rounded-md border py-2 px-8 text-center text-gray-500 transition duration-150 ease-in-out hover:translate-y-1 hover:bg-green-500 hover:text-white">+</button>
