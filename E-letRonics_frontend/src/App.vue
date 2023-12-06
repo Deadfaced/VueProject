496443448c4ba3870c9ccf082bf12b09ef9b6ad9
Bernardo Teixeira <btteixeira.bt@gmail.com>
2023-12-06 10:11:45 +0000

    added app.vue

---
+++ E-letRonics_frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from './components/Navigation/Navbar.vue'
+import Footer from './components/Navigation/Footer.vue'
+</script>
+
+<template>
+    <Navbar/>
+    <router-view></router-view>
+    <Footer/>
+</template>
+
+<style scoped>
+
+</style>
