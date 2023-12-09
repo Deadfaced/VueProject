419095cdd219b8046d58f7a843213f5bf61c6d79
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2023-12-09 15:58:02 +0000

    CheckoutCard trash button logic implemented

--- E-letRonics_frontend/src/components/Cart/SideCart.vue
+++ E-letRonics_frontend/src/components/Cart/SideCart.vue
@@ -70,9 +70,9 @@ export default {
         </li>
       </ul>
     </div>
-    <div class=" mt-auto py-6 space-x-2">
+    <div class=" mt-auto ml-auto py-6 space-x-2">
       <router-link :to="{ name: 'Checkout', params: { shoppingCart: shoppingCart } }"
-        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Checkout</router-link>
+        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mr-4">Checkout</router-link>
     </div>
   </div>
 </template>
