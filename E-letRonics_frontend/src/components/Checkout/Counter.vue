f9a5c2b1449a42f6c467a9010a51b0fa7e123cb7
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2023-12-13 09:30:36 +0000

    Rectification of the logic to decrement items in the navbar counter

--- E-letRonics_frontend/src/components/Checkout/Counter.vue
+++ E-letRonics_frontend/src/components/Checkout/Counter.vue
@@ -30,13 +30,12 @@ export default {
       }
       
       localStorage.setItem('cart', JSON.stringify(cart));
-      EventBus.emit('cart-updated');
       EventBus.emit('product-added-to-cart', { quantity: 1 });
     },
     decreaseQuantity() {
       if (this.quantity > 1) {
+
         let cart = JSON.parse(localStorage.getItem('cart')) || [];
-      
         let found = cart.find(el => el.id === this.id);
         
         if (found) {
@@ -45,15 +44,12 @@ export default {
           
         }
         localStorage.setItem('cart', JSON.stringify(cart));
-
-        EventBus.emit('cart-updated');
         EventBus.emit('product-removed-from-cart', { quantity: 1 });
         
       }
-      else if(this.quantity == 1){
+      else if(this.quantity === 1){
         
         let cart = JSON.parse(localStorage.getItem('cart')) || [];
-      
         let found = cart.find(el => el.id === this.id);
         
         if (found) {
@@ -62,8 +58,8 @@ export default {
         }
         
         localStorage.setItem('cart', JSON.stringify(cart));
+        EventBus.emit('product-removed-from-cart', { quantity: 1 });
       }
-      EventBus.emit('cart-updated');
     },
   },
 };
