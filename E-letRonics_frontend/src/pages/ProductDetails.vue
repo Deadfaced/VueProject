0f16f5a494481bd88b541e65438fcabe24f88697
daniel pereira <danielpereira22costa@gmail.com>
2024-01-03 18:55:44 +0000

    Visual changes

--- E-letRonics_frontend/src/pages/ProductDetails.vue
+++ E-letRonics_frontend/src/pages/ProductDetails.vue
@@ -7,60 +7,65 @@
         <img :src="product.image || 'https://via.placeholder.com/400'" alt="Product Image"
           class="object-cover object-center w-full h-64 lg:h-auto mx-auto mb-4">
 
-          <div class="flex justify-between mt-4">
-            <span class="text-lg font-bold text-3x1">{{ product.price }} €</span>
-            <span class="text-lg font-bold text-3x1">Rating: {{ product.rating }}.0</span>
-          </div>
+        <div class="flex justify-between mt-4">
+          <span class="text-lg font-bold text-3x1">{{ product.price }} €</span>
+          <span class="text-lg font-bold text-3x1">Rating: {{ product.rating }}.0</span>
+        </div>
 
-        <div class="flex items-center justify-end">
-          <button @click="decreaseQuantity" class="btn">-</button>
-          <span class="mx-4 text-2xl">{{ quantity }}</span>
-          <button @click="increaseQuantity" class="btn">+</button>
-          <div class="flex ml-4">
-            <button @click="addToCart"
-              class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center mx-5">Add
-              to Cart</button>
-            <router-link to="/Shop"
-              class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">Back
-              to Products</router-link>
+        <div class="flex items-center justify-between">
+          <router-link to="/Shop"
+            class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-2 rounded inline-flex items-center">Back
+            to Products</router-link>
+
+          <div class="flex gap-3">
+            <button @click="decreaseQuantity" class="btn">-</button>
+            <span class="text-2xl">{{ quantity }}</span>
+            <button @click="increaseQuantity" class="btn">+</button>
+            <div class="flex">
+              <button @click="addToCart"
+                class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-2 rounded inline-flex items-center">Add
+                to Cart</button>
+            </div>
           </div>
         </div>
         <ProductList />
       </div>
     </div>
     <h1 class="text-2xl font-bold text-center">Reviews</h1>
-  <div class="flex flex-col items-center mt-12">
-    <div class="flex-1 mb-4 review-card">
-      <div class="flex items-center p-8 rounded-lg shadow-lg bg-slate-700">
-        <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="User Avatar" class="rounded-full mr-4 w-16 h-16">
-        <div>
-          <p class="text-white-800 text-lg">"This product is amazing! It completely changed my life. I can't imagine my
-            day without it."</p>
-          <p class="italic text-white-600 mt-2">- John Doe</p>
+    <div class="flex flex-col items-center mt-12">
+      <div class="flex-1 mb-4 review-card">
+        <div class="flex items-center p-8 rounded-lg shadow-lg bg-slate-700">
+          <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="User Avatar" class="rounded-full mr-4 w-16 h-16">
+          <div>
+            <p class="text-white-800 text-lg">"This product is amazing! It completely changed my life. I can't imagine my
+              day without it."</p>
+            <p class="italic text-white-600 mt-2">- John Doe</p>
+          </div>
+        </div>
+      </div>
+      <div class="flex-1 mb-4 review-card-2">
+        <div class="flex items-center p-8 rounded-lg shadow-lg bg-slate-800">
+          <img src="https://randomuser.me/api/portraits/women/2.jpg" alt="User Avatar"
+            class="rounded-full mr-4 w-16 h-16">
+          <div>
+            <p class="text-white-800 text-lg">"I was skeptical at first, but after using this product, I'm a true
+              believer.
+              It exceeded my expectations."</p>
+            <p class="italic text-white-600 mt-2">- Jane Smith</p>
+          </div>
+        </div>
+      </div>
+      <div class="flex-1 review-card mb-8">
+        <div class="flex items-center p-8 rounded-lg shadow-lg bg-slate-700">
+          <img src="https://randomuser.me/api/portraits/men/3.jpg" alt="User Avatar" class="rounded-full mr-4 w-16 h-16">
+          <div>
+            <p class="text-white-800 text-lg">"Life before this product and after are two completely different things. I
+              highly recommend it!"</p>
+            <p class="italic text-white-600 mt-2">- Mike Johnson</p>
+          </div>
         </div>
       </div>
     </div>
-    <div class="flex-1 mb-4 review-card-2">
-      <div class="flex items-center p-8 rounded-lg shadow-lg bg-slate-800">
-        <img src="https://randomuser.me/api/portraits/women/2.jpg" alt="User Avatar" class="rounded-full mr-4 w-16 h-16">
-        <div>
-          <p class="text-white-800 text-lg">"I was skeptical at first, but after using this product, I'm a true believer.
-            It exceeded my expectations."</p>
-          <p class="italic text-white-600 mt-2">- Jane Smith</p>
-        </div>
-      </div>
-    </div>
-    <div class="flex-1 review-card mb-8">
-      <div class="flex items-center p-8 rounded-lg shadow-lg bg-slate-700">
-        <img src="https://randomuser.me/api/portraits/men/3.jpg" alt="User Avatar" class="rounded-full mr-4 w-16 h-16">
-        <div>
-          <p class="text-white-800 text-lg">"Life before this product and after are two completely different things. I
-            highly recommend it!"</p>
-          <p class="italic text-white-600 mt-2">- Mike Johnson</p>
-        </div>
-      </div>
-    </div>
-  </div>
   </div>
 </template>
 
@@ -84,7 +89,7 @@ export default {
   methods: {
     increaseQuantity() {
       if (this.quantity < this.product.quantity)
-      this.quantity += 1;
+        this.quantity += 1;
     },
     decreaseQuantity() {
       if (this.quantity > 1) {
--- E-letRonics_frontend/src/router.js
+++ E-letRonics_frontend/src/router.js
@@ -6,7 +6,7 @@ import Shop from './pages/Shop.vue';
 import ProductDetails from './pages/ProductDetails.vue';
 import AboutUs from './pages/AboutUs.vue';
 import Checkout from './pages/Checkout.vue';
-import ProductList from './components/Product/ProductList.vue';
+
 const routes = [
   {
     path: '/',
@@ -42,13 +42,6 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
-});
-
-router.beforeEach((to, from, next) => {
-  if (to.path !== from.path) {
-    EventBus.emit('close-sidecart');
-  }
-  next();
 });
 
 export default router;
